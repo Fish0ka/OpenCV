f53b2116a154ac35a3fad89a5283e48cc684d301
ILMNF <KuroMono21@gmail.com>
2024-07-15 18:08:50 +0300

    Update OpenCV_practica.py
    Обновлённая версия программы, которая находит соответствия по одному изображению относительно множества шаблонов.

--- OpenCV_practica.py
+++ OpenCV_practica.py
@@ -1,32 +1,37 @@
+import os
 import cv2
-
-img1 = cv2.imread('samples/0.jpg', 0)
-img2 = cv2.imread('input_images/0.jpg', 0)
 
 orb = cv2.ORB_create(nfeatures=1000)
 
-kp1, des1 = orb.detectAndCompute(img1, None)
-kp2, des2 = orb.detectAndCompute(img2, None)
+samples = os.listdir('samples')
 
-img_kp1 = cv2.drawKeypoints(img1, kp1, None)
+img2 = cv2.imread(f'input_images/0.jpg', 0)
+kp2, des2 = orb.detectAndCompute(img2, None)
 img_kp2 = cv2.drawKeypoints(img2, kp2, None)
 
-bf = cv2.BFMatcher()
-matches = bf.knnMatch(des1, des2, k=2)
+for s in range(len(samples)):
+    img1 = cv2.imread(f'samples/{s}.jpg', 0)
 
-good = []
-for m, n in matches:
-    if m.distance < 0.75 * n.distance:
-        good.append([m])
-print(len(good))
-img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=2)
+    kp1, des1 = orb.detectAndCompute(img1, None)
 
-if len(good) > 20:
-    img2 = cv2.imread('input_images/0.jpg', 1)
-    cv2.imwrite(f'bottles/bottle{len(good)}.jpg', img2)
-else:
-    img2 = cv2.imread('input_images/0.jpg', 1)
-    cv2.imwrite(f'another/junk{len(good)}.jpg', img2)
+    img_kp1 = cv2.drawKeypoints(img1, kp1, None)
 
-cv2.imshow('matching', img3)
-cv2.waitKey(0)
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+
+    good = list()
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good.append([m])
+    print(len(good))
+    img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=2)
+
+    if len(good) >= 20:
+        img2 = cv2.imread(f'input_images/0.jpg', 1)
+        path1 = f'bottles/bottle.jpg'
+        cv2.imwrite(path1, img2)
+        break
+    else:
+        img2 = cv2.imread(f'input_images/0.jpg', 1)
+        path2 = f'another/junk.jpg'
+        cv2.imwrite(path2, img2)
